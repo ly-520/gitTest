19872a36bd24b6bb10bbf08f5e0f90814baad9d8
liujitong <liujitong@yonrun.com>
2019-11-29 17:25:30 +0800

    Initial commit

---
+++ src/main/java/com/example/gittest/test1/Test1.java
@@ -0,0 +1,21 @@
+package com.example.gittest.test1;/**
+ * @author ljt
+ * @Title: Test1
+ * @ProjectName gittest
+ * @Description:
+ * @Version:
+ * @date 2019/11/29 17:24
+ */
+
+/**
+ * @author
+ * @description
+ * @date 2019/11/29
+ *
+ */
+public class Test1 {
+
+  public static void main(String[] args) {
+    System.out.println("1111111111111");
+  }
+}
---
+++ src/main/java/com/example/gittest/test2/Test2.java
@@ -0,0 +1,17 @@
+package com.example.gittest.test2;/**
+ * @author ljt
+ * @Title: Test2
+ * @ProjectName gittest
+ * @Description:
+ * @Version:
+ * @date 2019/11/29 17:24
+ */
+
+/**
+ * @author
+ * @description
+ * @date 2019/11/29
+ *
+ */
+public class Test2 {
+}
